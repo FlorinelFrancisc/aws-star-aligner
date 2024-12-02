1b7b58daa345e9eccd83e7b8bab3a31196cf4716
FlorinelFrancisc <francisc_florinel@yahoo.com>
2024-12-02 15:18:34 +0100

    changes to match with previous template

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 sam validate --lint
 sam build
-sam package --region eu-north-1 --template-file template.yaml --output-template-file sam-package-output-file.yaml
+sam package --region eu-north-1 --template-file template.yaml --output-template-file sam-package-output-file.yaml --s3-bucket my-cloudformation-batch-bucket-2024-11-24/
 sam deploy --template-file sam-package-output-file.yaml --stack-name star-aligner --region eu-north-1 --capabilities CAPABILITY_NAMED_IAM
